cf9ec06323df2de42a8af315bcdd293b04993dff
SadegH <sadegh.iraji@ymail.com>
2021-10-23 20:17:01 +0330

    Add @Entity to Admin

--- src/main/java/ir/maktab/busticket/model/Admin.java
+++ src/main/java/ir/maktab/busticket/model/Admin.java
@@ -2,6 +2,9 @@ package ir.maktab.busticket.model;
 
 import ir.maktab.busticket.model.enumeration.UserType;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Admin extends User{
 
     public Admin() {
